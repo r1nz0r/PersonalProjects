6ecaaf8bfb90981e8feb13a35e3f5e2760dfab5b
Alexey Boitsov <nicecontra1@gmail.com>
2024-05-10 01:11:08 +0300

    Documentation for Event
    Added basic documentation for Event class.

--- SnakeGame/Event.h
+++ SnakeGame/Event.h
@@ -2,21 +2,74 @@
 #include <functional>
 #include <vector>
 
+
+/**
+* @brief Класс для реализации системы событий, позволяющей подписываться, отписываться и уведомлять обработчики событий.
+*
+* @tparam Args Аргументы, которые передаются обработчикам событий.
+*/
 template <typename... Args>
 class Event
 {
 public:
+	/**
+	* @brief Деструктор, который автоматически отписывает все обработчики.
+	*/
+	~Event();
+
+	/**
+	* @brief Тип функции, представляющей обработчик события.
+	*/
 	using EventHandler = std::function<void(Args...)>;
+
+	/**
+	* @brief Подписывает новый обработчик на событие.
+	* @param handler Функция-обработчик, которая будет вызвана при срабатывании события.
+	*/
 	void Subscribe(EventHandler handler);
+
+	/**
+	* @brief Отписывает обработчик от события.
+	* @param handler Функция-обработчик, которая больше не должна вызываться.
+	*/
 	void Unsubscribe(EventHandler handler);
+
+	/**
+	* @brief Уведомляет все подписанные обработчики о событии.
+	* @param args Аргументы, передаваемые в обработчики.
+	*/
 	void Notify(Args... args);// Notify all listeners
+
+	/**
+	* @brief Оператор добавления обработчика (перегрузка +=).
+	* @param handler Функция-обработчик для добавления.
+	* @return Ссылка на текущий объект `Event`.
+	*/
 	Event<>& operator+=(EventHandler handler);// Call Subscribe()
+
+	/**
+	* @brief Оператор удаления обработчика (перегрузка -=).
+	* @param handler Функция-обработчик для удаления.
+	* @return Ссылка на текущий объект `Event`.
+	*/
 	Event<>& operator-=(EventHandler handler);// Call Unsubscribe()
 	void operator()(Args... args);// Call Notify()
 
 private:
+	/**
+	* @brief Хранилище всех подписанных обработчиков событий.
+	*/
 	std::vector<EventHandler> _handlers;
 };
+
+template<typename ...Args>
+inline Event<Args...>::~Event()
+{
+	for (auto& handler : _handlers)
+	{
+		Unsubscribe(handler);
+	}
+}
 
 template<typename ...Args>
 inline void Event<Args...>::Subscribe(EventHandler handler)
