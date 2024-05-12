4ad46bab901e36cec5f5c53f01172534a13df7b5
Alexey Boitsov <nicecontra1@gmail.com>
2024-05-12 20:55:57 +0300

    Added Menu class functionality
    Added functionality for Menu class.

--- SnakeGame/Menu.cpp
+++ SnakeGame/Menu.cpp
@@ -1,1 +1,125 @@
 #include "Menu.h"
+#include "GameSettings.h"
+
+Menu::Menu
+(
+	ItemsList items,
+	const sf::Font& font,
+	sf::RenderWindow& window,
+	std::string title,
+	const float verticalSpacing,
+	Menu* root,
+	const sf::Color selectedColor,
+	const sf::Color unselectedColor
+) :	_items(items),
+	_font(font),
+	_window(window),
+	_root(root),
+	_selectedItemIndex(0),
+	_selectedItemColor(selectedColor),
+	_unselectedItemColor(unselectedColor)
+{
+	SetupText(verticalSpacing);
+	SetupTitle(title);
+}
+
+void Menu::Draw(sf::RenderWindow& window)
+{
+	_title.Draw(window);
+	_itemsBlock.Draw(window);
+}
+
+void Menu::HandleInput(const sf::Event& event)
+{
+	switch (event.key.code)
+	{
+	case sf::Keyboard::W:
+		MoveUp();
+		break;
+	case sf::Keyboard::S:
+		MoveDown();
+		break;
+	case sf::Keyboard::Return:
+		ExecuteSelectedItem();
+		break;
+	default:
+		break;
+	}
+}
+
+void Menu::SetRootItem(Menu* root)
+{
+	_root = root;
+}
+
+void Menu::SetMenuItemsAlignment(const TextBlock::Alignment horizontal, const TextBlock::Alignment vertical, const Text::Alignment textHorizontal)
+{
+	_itemsBlock.SetAlignmentProperties(horizontal, vertical, textHorizontal);
+	_itemsBlock.AlignTexts();
+}
+
+Menu* Menu::GetRootItem() const
+{
+	return _root;
+}
+
+void Menu::MoveUp()
+{
+	if (_selectedItemIndex > 0)
+	{
+		_itemsBlock.SetColor(_selectedItemIndex, _unselectedItemColor);
+		_selectedItemIndex--;
+		_itemsBlock.SetColor(_selectedItemIndex, _selectedItemColor);
+	}
+}
+
+void Menu::MoveDown()
+{
+	const int MAX_INDEX = _items.size() - 1;
+
+	if (_selectedItemIndex < MAX_INDEX)
+	{
+		_itemsBlock.SetColor(_selectedItemIndex, _unselectedItemColor);
+		_selectedItemIndex++;
+		_itemsBlock.SetColor(_selectedItemIndex, _selectedItemColor);
+	}
+}
+
+void Menu::ExecuteSelectedItem()
+{
+	if (_selectedItemIndex >= 0 && _selectedItemIndex < _items.size())
+	{
+		if (_items[_selectedItemIndex].second)
+		{
+			_items[_selectedItemIndex].second();
+		}
+	}
+}
+
+void Menu::SetupText(const float verticalSpacing)
+{
+	
+	_itemsBlock.Clear(); // Очищаем все предыдущие тексты, если они были
+	_itemsBlock.SetStartPosition(sf::Vector2f(_window.getSize().x / 2, _window.getSize().y / 2)); // Устанавливаем начальную позицию блока текста
+	_itemsBlock.SetVerticalSpacing(verticalSpacing); // Устанавливаем вертикальный интервал между текстами
+
+	for (const auto& item : _items)	
+		_itemsBlock.AddText(item.first, _font, sf::Color::White, 25); // Добавляем текст в блок	
+
+	if (!_items.empty())
+		_itemsBlock.SetColor(0, _selectedItemColor);
+
+	_itemsBlock.AlignTexts();
+}
+
+void Menu::SetupTitle(std::string& text)
+{
+	_title.setFont(_font);
+	_title.setString(text);
+	_title.setCharacterSize(30);
+	float titleOffsetY = 25.0f;
+	sf::Vector2f position { _window.getSize().x / 2.0f, titleOffsetY };
+	_title.setPosition(position);
+	_title.Align(position);
+}
+
--- SnakeGame/Menu.h
+++ SnakeGame/Menu.h
@@ -1,4 +1,50 @@
 #pragma once
-class Menu
-{};
+#include <vector>
+#include <functional>
+#include "SFML/Graphics.hpp"
+#include "IDrawable.h"
+#include "TextBlock.h"
+
+class Menu : public IDrawable
+{
+public:
+	using ItemCallback = std::function<void(void)>;
+	using Item = std::pair<std::string, ItemCallback>;
+	using ItemsList = std::vector<Item>;
+
+	Menu
+	(
+		ItemsList items,
+		const sf::Font& font,
+		sf::RenderWindow& window,
+		std::string title = "",
+		const float verticalSpacing = 10.0f,
+		Menu* root = nullptr,
+		const sf::Color selectedColor = sf::Color::Green,
+		const sf::Color unselectedColor = sf::Color::White
+	);
+	virtual ~Menu() {}
+	virtual void Draw(sf::RenderWindow& window) override;
+	void HandleInput(const sf::Event& event);
+	void SetRootItem(Menu* root);
+	void SetMenuItemsAlignment(const TextBlock::Alignment horizontal, const TextBlock::Alignment vertical, const Text::Alignment textHorizontal);
+	Menu* GetRootItem() const;
+
+private:
+	ItemsList _items;
+	TextBlock _itemsBlock;
+	Text _title;
+	sf::Color _selectedItemColor;
+	sf::Color _unselectedItemColor;
+	sf::Font _font;
+	sf::RenderWindow& _window;
+	int _selectedItemIndex;
+	Menu* _root;
+
+	void MoveUp();
+	void MoveDown();
+	void ExecuteSelectedItem();
+	void SetupText(const float verticalSpacing);
+	void SetupTitle(std::string& text);
+};
 
